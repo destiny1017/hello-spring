26d830d80774e8fce4342e7b7753cf7678eb2300
KDH_Notebook <destiny1017@nate.com>
2022-03-29 21:24:12 +0900

    spring Data JPA 예제

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,37 +1,44 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
-    private EntityManager em;
+
+    // Spring Data JPA Config
+    private final MemberRepository memberRepository;
 
     @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // Spring JPA Config
+/*    private DataSource dataSource;
+    private EntityManager em;
+
+   @Autowired
     public SpringConfig(DataSource dataSource, EntityManager em) {
         this.em = em;
-    }
+    }*/
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+    // Spring Data JPA 사용 시에는 필요하지않음
+/*    @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
         return new JpaMemberRepository(em);
-    }
+    }*/
 
 }
